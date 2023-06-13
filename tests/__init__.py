a8dbebe560e213dc87dd4385d05b4ebe430c20d0
cybershiptrooper <cybershiptrooper@gmail.com>
2023-06-13 18:45:39 +0530

    add unique test_reponses

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+import os
+test_responses_dir = os.path.join(os.path.dirname(__file__), "../storage/test_responses")
+pdfs_dir = os.path.join(os.path.dirname(__file__), "../storage/test_pdfs")
--- tests/test_chains.py
+++ tests/test_chains.py
@@ -1,13 +1,14 @@
 from utils.chains import *
-import os
-test_responses_dir = os.path.join(os.path.dirname(__file__), "../storage/test_responses")
-pdfs_dir = os.path.join(os.path.dirname(__file__), "../storage/test_pdfs")
+import time
+from . import *
+
 def test_generate_db_from_pdf():
     pdf_path = os.path.join(pdfs_dir, "CS_781_Project.pdf")
     generator = ContextBasedGenerator(pdf_path)
     reponse = generator.generate_chain_response("Make an mcq quiz form with 3 questions in it.")
     try:
-        with open(os.path.join(test_responses_dir, "test_generate_db_from_pdf.html"), "w") as f:
+        time_ext = time.clock_gettime_ns()
+        with open(os.path.join(test_responses_dir, f"test_generate_db_from_pdf_{time_ext}.html"), "w") as f:
             f.write(reponse[0]['text'])
     except:
         print(reponse)
