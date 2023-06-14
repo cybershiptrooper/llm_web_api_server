74c6004b955565278c1ca4f7e269c6b2199368b7
cybershiptrooper <cybershiptrooper@gmail.com>
2023-06-15 02:39:03 +0530

    Working pdf services APIs but not integrated

--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,9 @@
 from .routes_impl import *
 from .debug_routes import *
 from .pdf_routes import *
+import json
 
+html_sample=".\n\n<!DOCTYPE html>\n<html>\n<head>\n<style>\nh1 {\n    font-family: sans-serif;\n    font-size: 1.5em;\n    text-align: center;\n    color: #3A3A3A;\n}\n\nform {\n    font-family: sans-serif;\n    font-size: 1em;\n    color: #3A3A3A;\n    padding: 10px;\n    border: 1px solid #3A3A3A;\n    border-radius: 5px;\n    margin: 0 auto;\n    width: 400px;\n}\n\ninput[type=text], select {\n    width: 100%;\n    padding: 12px 20px;\n    margin: 8px 0;\n    display: inline-block;\n    border: 1px solid #ccc;\n    border-radius: 4px;\n    box-sizing: border-box;\n}\n\ninput[type=submit] {\n    width: 100%;\n    background-color: #3A3A3A;\n    color: white;\n    padding: 14px 20px;\n    margin: 8px 0;\n    border: none;\n    border-radius: 4px;\n    cursor: pointer;\n}\n\ninput[type=submit]:hover {\n    background-color: #454545;\n}\n\ndiv {\n    border-radius: 5px;\n    background-color: #f2f2f2;\n    padding: 20px;\n}\n</style>\n</head>\n<body>\n\n<h1>BeneFit Customer Acquisition Form</h1>\n\n<div>\n  <form action=\"/action_page.php\">\n    <label for=\"name\">Name</label>\n    <input type=\"text\" id=\"name\" name=\"name\" placeholder=\"Your name..\">\n\n    <label for=\"email\">Email</label>\n    <input type=\"text\" id=\"email\" name=\"email\" placeholder=\"Your email..\">\n\n    <label for=\"phone\">Phone Number</label>\n    <input type=\"text\" id=\"phone\" name=\"phone\" placeholder=\"Your phone number..\">\n\n    <label for=\"membership\">Membership Type</label>\n    <select id=\"membership\" name=\"membership\">\n      <option value=\"basic\">Basic</option>\n      <option value=\"premium\">Premium</option>\n    </select>\n\n    <input type=\"submit\" value=\"Submit\">\n  </form>\n</div>\n\n</body>\n</html>"
 # create a route to prompt gpt-3 to generate a response 
 # the key is stored in env.OPENAI_API_KEY
 @app.route("/generate", methods=["POST"])
@@ -68,3 +70,74 @@ def generate_html_response():
     except Exception as e:
         print(e)
         return make_response("Error generating response", HTTPStatus.INTERNAL_SERVER_ERROR)
+
+@app.route("/get_pdf_from_pdf", methods=["POST"])
+def get_pdf_from_pdf():
+    # get prompt from request body
+    prompt = request.form["prompt"]
+    
+    # download pdf from url
+    # pdf_url = request.json["pdf_url"]
+    # pdf_path = download_pdf(pdf_url)
+    file = request.files['file']
+    pdf_path = get_pdf_from_client(file)
+    
+    # generate response
+    try:
+        gpt_response = get_gpt_response(prompt, pdf_path)
+    except Exception as e:
+        print(e)
+        return make_response("Error generating response", HTTPStatus.INTERNAL_SERVER_ERROR)
+    write_html(gpt_response)
+    # call extra processors if needed
+    pdf = html2pdf(gpt_response)
+    return make_response(pdf, HTTPStatus.OK)
+
+@app.route("/get_url", methods=["GET"])
+def get_url():
+    # generate response
+    try:
+        response = get_upload_url()
+        print(response)
+    except Exception as e:
+        print(e)
+        return make_response("Error generating response", HTTPStatus.INTERNAL_SERVER_ERROR)
+    return make_response(response.content, HTTPStatus.OK)
+
+@app.route("/get_document_from_html", methods=["GET"])
+def get_document_from_html():
+    # generate response
+    try:
+        upload_url_response = get_upload_url()
+        upload_url_response_json = json.loads(upload_url_response.content)
+        # print(upload_url_response.content)
+        # print(upload_url_response_json)
+        # Extract the value from the JSON response
+        #value = json_response.get('value')
+        # Return the extracted value
+        html_string = replace_form_action(html_sample, 'mailto:nikhilarora@adobe.com')
+        asset_id = upload_url_response_json.get('assetID')
+        uploadUri = upload_url_response_json.get('uploadUri')
+        # print(uploadUri)
+        upload_html_response = upload_html(uploadUri, html_string)
+        print(upload_html_response.content)
+        # print("Printing asset ID next")
+        # print(asset_id)
+        download_url_response = get_download_url(asset_id)
+        download_url_response_json = json.loads(download_url_response.content)
+        downloadUri = download_url_response_json.get('downloadUri')
+        print("Printing downloadUri next")
+        print(downloadUri)
+        html_to_pdf_response = schedule_html_to_pdf_conversion(downloadUri)
+        html_to_pdf_response_headers = dict(html_to_pdf_response.headers)
+        print("html to pdf api response")
+        print(html_to_pdf_response.content)
+        print("Printing headers next")
+        print(html_to_pdf_response_headers)
+        poll_uri = html_to_pdf_response_headers.get('location')    
+        polling_response_json = poll_html_to_pdf_conversion(3, poll_uri)
+
+    except Exception as e:
+        print(e)
+        return make_response("Error generating response", HTTPStatus.INTERNAL_SERVER_ERROR)
+    return make_response(polling_response_json, HTTPStatus.OK)
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -2,6 +2,11 @@ import requests
 import re
 from . import *
 from datetime import datetime
+import json
+import time
+
+client_id="38c17d66d1ef4e9b96f5d094cdd1b6e3"
+token="eyJhbGciOiJSUzI1NiIsIng1dSI6Imltc19uYTEta2V5LWF0LTEuY2VyIiwia2lkIjoiaW1zX25hMS1rZXktYXQtMSIsIml0dCI6ImF0In0.eyJpZCI6IjE2ODY3MjcxMTk1NzZfMWIxY2E2MzEtMTEyZi00YjVjLWFiNjgtMGNmNjY2YThiOWQ5X3V3MiIsInR5cGUiOiJhY2Nlc3NfdG9rZW4iLCJjbGllbnRfaWQiOiIzOGMxN2Q2NmQxZWY0ZTliOTZmNWQwOTRjZGQxYjZlMyIsInVzZXJfaWQiOiJGNUIxMUVERjY0ODk2OTQwMEE0OTVFMzRAdGVjaGFjY3QuYWRvYmUuY29tIiwiYXMiOiJpbXMtbmExIiwiYWFfaWQiOiJGNUIxMUVERjY0ODk2OTQwMEE0OTVFMzRAdGVjaGFjY3QuYWRvYmUuY29tIiwiY3RwIjozLCJmZyI6IlhRWjVNWUk0RlBQNU1QNEtFTVFWWkhBQUpVPT09PT09IiwibW9pIjoiZmU4MDEyNTUiLCJleHBpcmVzX2luIjoiODY0MDAwMDAiLCJzY29wZSI6Im9wZW5pZCxEQ0FQSSxBZG9iZUlELGFkZGl0aW9uYWxfaW5mby5vcHRpb25hbEFncmVlbWVudHMiLCJjcmVhdGVkX2F0IjoiMTY4NjcyNzExOTU3NiJ9.ANIBP2SAPZ7vhm8weGkpQGvQ2FaHv4An2C0bhVjB9_tKHB3hSzIo9PyeBPuMyRkqfYBFEYP8DF-IFnbLQRyAmZIcagjgX0nHniSDSOrgEHdnUHuFK380zIQKvYMwCTjLEKTJpfV1oOMaaUow6gKz7yjFLfgalqsAx9gcaa8QLeO0tCS6kdFx4Rx_TNqJfdKZlRK8r1NCLk5j0YldEMo74ftywfm6V73EvEf8fpRvXDGKnojq7qhcMs3ZeTg7frG_KBeg5f61B79C6FwJGBqyJfo0tKeN5VUM803K5vQ8V7A6l8uSAcBACulWcnHnpoXVs3KZ2I4w8T07Lk9Xcq0UAA"
 
 def download_pdf(url, chunk_size = 2000):
   r = requests.get(url, stream=True)
@@ -20,7 +25,7 @@ def replace_form_action(html_data, new_action):
   pattern = r'<form\s+[^>]*\baction\s*=\s*["\']([^"\']*)["\']'
   repl = '<form action="{}"'.format(new_action)
   updated_html = re.sub(pattern, repl, updated_html, count=1)
-  print(updated_html)
+  # print(updated_html)
   return updated_html
 
 def html2pdf(html_string):
@@ -53,3 +58,94 @@ def write_html(html_string):
    file_name = datetime.now().strftime("%d_%m_%Y_%H_%M_%S") + ".html"
    with open(os.path.join(responses_dir, file_name), "w") as f:
         f.write(html_string)
+
+def get_upload_url():
+  url = "https://pdf-services.adobe.io/assets"
+  data = {
+    "mediaType": "text/html"
+  }
+  headers = {
+    "X-API-Key": client_id,
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
+  }
+  response = requests.post(url, json=data, headers=headers)
+  return response
+
+def upload_html(uploadUri, html_data):
+  headers = {
+    "Content-Type": "text/html",
+  }
+  response = requests.put(uploadUri, data=html_data, headers=headers)
+  return response
+  
+def get_download_url(asset_id):
+  url = "https://pdf-services.adobe.io/assets/" + asset_id
+  headers = {
+    "X-API-Key": client_id,
+    "Authorization": "Bearer " + token,
+  }
+  response = requests.get(url, headers=headers)
+  return response 
+
+def schedule_html_to_pdf_conversion(download_url):
+  url = "https://pdf-services-ue1.adobe.io/operation/htmltopdf"
+  data = {
+    "inputUrl": download_url,
+    "json": "{}",
+    "pageLayout": {
+      "pageWidth": 11,
+      "pageHeight": 8.5
+    }
+  }
+  headers = {
+    "X-API-Key": client_id,
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
+  }
+  response = requests.post(url, json=data, headers=headers)
+  return response
+
+def status_html_to_pdf_conversion(polling_url):
+  headers = {
+    "X-API-Key": client_id,
+    "Authorization": "Bearer " + token,
+  }
+  response = requests.get(polling_url, headers=headers)
+  return response 
+
+def poll_html_to_pdf_conversion(polling_time_in_seconds, polling_url):
+    while True:
+        print("Polling...")
+        status_response = status_html_to_pdf_conversion(polling_url)
+        status_response_json = json.loads(status_response.content)
+        status = status_response_json.get('status')
+        if status != "in progress":
+            return status_response_json
+        time.sleep(polling_time_in_seconds)
+
+# def html2pdfNew(html_string):
+#   url = "https://yakpdf.p.rapidapi.com/pdf"
+
+#   payload = {
+#     "source": { "html": f"{html_string}" },
+#     "pdf": {
+#       "format": "A4",
+#       "scale": 1,
+#       "printBackground": True
+#     },
+#     "wait": {
+#       "for": "navigation",
+#       "waitUntil": "load",
+#       "timeout": 2500
+#     }
+#   }
+#   headers = {
+#     "content-type": "application/json",
+#     "x-api-key": "none",
+#     "X-RapidAPI-Key": "b4cd2bfc17msh566656031a54132p121277jsn1bb63ca95415",
+#     "X-RapidAPI-Host": "yakpdf.p.rapidapi.com"
+#   }
+
+#   response = requests.post(url, json=payload, headers=headers)
+#   return response.content
