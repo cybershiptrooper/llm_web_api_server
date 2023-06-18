07ec9de447cf87328e04cdf3f71e88f2e29f8920
cybershiptrooper <cybershiptrooper@gmail.com>
2023-06-19 00:21:26 +0530

    Update credentials

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,8 +6,8 @@ import os
 # import subprocess
 # import tempfile
 
-openai.api_key = "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"
-os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 # file directories
 db_dir = os.path.join(os.path.dirname(__file__), "../storage/db")
