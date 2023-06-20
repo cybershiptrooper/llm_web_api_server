03109f35de630fc9339aa6ac1825d18d60eb3a61
cybershiptrooper <cybershiptrooper@gmail.com>
2023-06-21 03:32:56 +0530

    Update gpt token

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,8 +6,8 @@ import os
 # import subprocess
 # import tempfile
 
-openai.api_key = "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"
-os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 # file directories
 db_dir = os.path.join(os.path.dirname(__file__), "../storage/db")
